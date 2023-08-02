9e5bb6691c4ba83302ecd8d53c1e0cfcab005228
Koseng <26673978+Koseng@users.noreply.github.com>
2023-08-02 20:53:37 +0200

    Set version to 0.7.1

--- src/Sources/Code/Module.cpp
+++ src/Sources/Code/Module.cpp
@@ -11,7 +11,7 @@
 #include "Module.h"
 
 HANDLE g_hSimConnect;
-const char* version = "0.6.0";
+const char* version = "0.7.1";
 
 const char* ClientName = "MobiFlightWasmModule";
 const char* MobiFlightEventPrefix = "MobiFlight.";
